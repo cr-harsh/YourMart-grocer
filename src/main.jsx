8cdd5fd5324df5c5695282d0e0bd13950d86d5f5
cr-harsh <harshbhuteja56@gmail.com>
2026-01-13 18:41:59 +0530

    fixed again

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import { ToastContainer } from 'react-toastify'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/YourMart/">
+    <BrowserRouter>
       <Provider store={store}>
         <App />
         <ToastContainer
